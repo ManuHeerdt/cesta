737635955e67c215b109f477fb91b3a4ab9d2602
Manu Heerdt <emanuelly.heerdt@escola.pr.gov.br>
2026-02-09 13:07:18 +0000

    aula 1

---
+++ App.js
@@ -0,0 +1,17 @@
+
+import { Text, SafeAreaView } from 'react-native';
+import Sobrenome from './src/Telas/Sobrenome';
+
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Text>Emanuelly</Text>
+      <Sobrenome  sobrenome="Heerdt"/>
+    </SafeAreaView>
+  )
+
+
+}
+
+
